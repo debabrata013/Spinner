47622e0e6625b2c3deded6c395008de0c7dbd992
DEV <pattnaikd833@gmail.com>
2025-02-27 18:54:15 +0530

    initalized the spinner in MainActivity

--- app/src/main/java/com/example/spinner/MainActivity.kt
+++ app/src/main/java/com/example/spinner/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.spinner
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
+ val contries =resources.getStringArray(R.array.country)
+        val spinner = findViewById<Spinner>(R.id.sel_Country)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, contries)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 }
