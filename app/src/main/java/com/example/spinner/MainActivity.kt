41c28dcc80d8ec78ef554eccaab6edbf950de076
DEV <pattnaikd833@gmail.com>
2025-02-27 19:21:44 +0530

    last step Set Item Selection Listener: Implement the  to handle user selections:

--- app/src/main/java/com/example/spinner/MainActivity.kt
+++ app/src/main/java/com/example/spinner/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.spinner
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewParent
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,7 +20,17 @@ class MainActivity : AppCompatActivity() {
  val contries =resources.getStringArray(R.array.country)
         val spinner = findViewById<Spinner>(R.id.sel_Country)
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, contries)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
+
+        spinner.onItemSelectedListener= object : AdapterView.OnItemSelectedListener {
+          override  fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+               Toast.makeText(this@MainActivity, "You selected ${contries[position]}", Toast.LENGTH_SHORT).show()
+
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+//                //TODO("Not yet implemented")
+            }
+        }
     }
 }
